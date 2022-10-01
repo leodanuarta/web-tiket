973306e7b37be1cdd551c41edbf47c4b9186d350
Angeelll1 <111833797+Angeelll1@users.noreply.github.com>
2022-10-01 23:27:03 +0700

    semi-final

--- pesawat/pesawat.js
+++ pesawat/pesawat.js
@@ -1,8 +1,28 @@
-$('#datepicker').datepicker();
+// $('#datepicker').datepicker();
+// $('#datepicker2').datepicker();
 
+$(document).ready(function (){
+    var minDate = new Date();
+    $("#depart").datepicker({
+        showAnim: "drop",
+        numberOfMonth: 1,
+        minDate: minDate,
+        dateFormat: 'dd/mm/yyyy',
+        onClose: function(selectedDate){
+            $('#return').datepicker("option", "minDate", selectedDate);
+        }
+    });
 
-$('#datepicker2').datepicker();
-
+    $("#return").datepicker({
+        showAnim: "drop",
+        numberOfMonth: 1,
+        minDate: minDate,
+        dateFormat: 'dd/mm/yyyy',
+        onClose: function(selectedDate){
+            $('#depart').datepicker("option", "minDate", selectedDate);
+        }
+    });
+});
 
 // var disPul = $('#datepicker').disabled();
 
