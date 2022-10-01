451cd7582656438ae185201768fe93dcb43909b2
Angeelll1 <111833797+Angeelll1@users.noreply.github.com>
2022-10-01 18:11:45 +0700

    disabled

---
+++ pesawat/pesawat.js
@@ -0,0 +1,15 @@
+$('#datepicker').datepicker();
+
+
+$('#datepicker2').datepicker();
+
+
+// var disPul = $('#datepicker').disabled();
+
+function disPul(){
+    document.getElementById("plg1").style.visibility="hidden"; 
+} 
+
+function pul(){
+    document.getElementById("plg1").style.visibility="visible";
+}
