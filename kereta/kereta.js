3f26a98b8639ab291db9d2ded3357a5b830b6c87
Angeelll1 <111833797+Angeelll1@users.noreply.github.com>
2022-10-01 23:32:28 +0700

    semi-final

--- kereta/kereta.js
+++ kereta/kereta.js
@@ -1,3 +1,26 @@
-$('#datepicker').datepicker();
+// $('#datepicker').datepicker();
 
-$('#datepicker2').datepicker();
+// $('#datepicker2').datepicker();
+
+$(document).ready(function (){
+    var minDate = new Date();
+    $("#depart").datepicker({
+        showAnim: "drop",
+        numberOfMonth: 1,
+        minDate: minDate,
+        dateFormat: 'dd/mm/yyyy',
+        onClose: function(selectedDate){
+            $('#return').datepicker("option", "minDate", selectedDate);
+        }
+    });
+
+    $("#return").datepicker({
+        showAnim: "drop",
+        numberOfMonth: 1,
+        minDate: minDate,
+        dateFormat: 'dd/mm/yyyy',
+        onClose: function(selectedDate){
+            $('#depart').datepicker("option", "minDate", selectedDate);
+        }
+    });
+});
