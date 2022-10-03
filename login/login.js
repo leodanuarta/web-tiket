8f80cd4c1276b7826e56058d4e62dd39713d54d1
Leo Danuarta <99653767+leodanuarta@users.noreply.github.com>
2022-10-03 23:43:30 +0700

    update login.js SemiFinal

--- login/login.js
+++ login/login.js
@@ -41,17 +41,20 @@ $(function() {
             lastname: "required",
             dob: "required",
             notel: "required",
-            email: {
+            email1: {
                 required: true,
                 email: true
             },
-            password: {
+            password1: {
                 required: true,
                 minlength: 8
             },
-            password2: {
-                required: true,
-                minlength : 8
+            // confirm_password: {
+            //     required: true,
+            //     minlength : 8
+            // },
+            confirm_password: {
+                required : true
             },
             tac : {
                 required : true,
@@ -63,16 +66,17 @@ $(function() {
             lastname: "Masukkan Nama Belakang",
             dob: "Pilih Tanggal Lahir Anda",
             notel: "Masukkan Nomor Telepon Anda",
-            password: {
+            password1: {
                 required: "Masukkan Password",
                 minlength: "Minimal Password adalah 8 karakter"
             },
-            password2: {
-                required: "Ulangi Masukkan Password",
-                minlength: "Minimal Password adalah 8 karakter"
-            },
+            // confirm_password: {
+            //     required: "Ulangi Masukkan Password",
+            //     minlength: "Minimal Password adalah 8 karakter"
+            // },
+            confirm_password: "Ulangi Masukkan Password",
             tac : "Mohon di klik!",
-            email: "Mohon Masukkan Email yang Valid!"
+            email1: "Mohon Masukkan Email yang Valid!"
         },
 
         submitHandler: function(form){
@@ -88,12 +92,70 @@ function forgotPassword(){
     }
 };
 
-function onChange() {
-    const password = document.querySelector('input[name=password]');
-    const confirm = document.querySelector('input[name=password2]');
-    if (confirm.value === password.value) {
-      confirm.setCustomValidity('');
-    } else {
-      confirm.setCustomValidity('Passwords do not match');
+// function onChange() {
+//     const password = document.querySelector('input[name=password1]');
+//     const confirm = document.querySelector('input[name=confirm_password]');
+//     if (confirm.value === password.value) {
+//       confirm.setCustomValidity('');
+//     } else {
+//       confirm.setCustomValidity('Passwords do not match');
+//     }
+// }
+
+$(document).ready(function(){
+    $("#confirm_password").bind('keyup change', function(){
+      check_Password( $("#password1").val(), $("#confirm_password").val() )
+
+    $("#btn-daf").click(function(){
+      check_Password( $("#password1").val(), $("#confirm_password").val() )
+    })
+  })
+
+  function check_Password( Pass, Con_Pass){
+    if(Pass === ""){
+
+    }else if( Pass === Con_Pass){
+      $("#btn-daf").removeAttr("onclick")
+      $('#confirm_password_msg').show()
+    //   $("#confirm_password_msg").html('<div class="alert alert-success">Password matched</div>')
+    $("#confirm_password_msg").html('<div class="text-success">Password matched</div>')
+    }else{
+      $("#confirm_password").focus()
+      $('#confirm_password_msg').show()
+      $("#confirm_password_msg").html('<div class="text-danger">Password not matched!</div>')
     }
-}
+  }
+})
+
+$("#eye").click(function() {
+	var password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+    $('#eye').toggleClass("fa-eye fa-eye-slash");
+  } else {
+    password.type = "password";
+    $('#eye').toggleClass("fa-eye-slash fa-eye");
+  }
+});
+
+$("#eye1").click(function() {
+	var password = document.getElementById("password1");
+  if (password.type === "password") {
+    password.type = "text";
+    $('#eye1').toggleClass("fa-eye fa-eye-slash");
+  } else {
+    password.type = "password";
+    $('#eye1').toggleClass("fa-eye-slash fa-eye");
+  }
+});
+
+$("#eye2").click(function() {
+	var password = document.getElementById("confirm_password");
+  if (password.type === "password") {
+    password.type = "text";
+    $('#eye2').toggleClass("fa-eye fa-eye-slash");
+  } else {
+    password.type = "password";
+    $('#eye2').toggleClass("fa-eye-slash fa-eye");
+  }
+});
