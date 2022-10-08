573a04eb6e96db1da87bbc3c1c81190eb791ccdf
Leo Danuarta <99653767+leodanuarta@users.noreply.github.com>
2022-10-08 23:03:06 +0700

    Update tiketpesawat.js

--- tiket pesawat/tiketpesawat.js
+++ tiket pesawat/tiketpesawat.js
@@ -1,23 +1,20 @@
 window.onscroll = function () {
     scrollFunction();
   };
-
-function scrollFunction() {
+  
+  function scrollFunction() {
     if (
       document.body.scrollTop > 100 ||
       document.documentElement.scrollTop > 100
     ){
-      // secnav.style.display = "block";
       firstnav.style.display = "none";
-      // secnav.style.position = "fixed";
     }else {
-      // secnav.style.display = "none";
       firstnav.style.display = "block";
     }
-}
-
-var checkList1 = document.getElementById('list1');
-var items1 = document.getElementById('items1');
+  }
+  
+  var checkList1 = document.getElementById('list1');
+  var items1 = document.getElementById('items1');
   checkList1.getElementsByClassName('anchor')[0].onclick = function (evt) {
     if (items1.classList.contains('visible')){
       items1.classList.remove('visible');
@@ -31,14 +28,14 @@ var items1 = document.getElementById('items1');
   items1.onblur = function(evt) {
     items1.classList.remove('visible');
   }
-
-window.onscroll = function () {
+  
+  window.onscroll = function () {
     scrollFunction();
   };
-
-
-var checkList2 = document.getElementById('list2');
-var items2 = document.getElementById('items2');
+  
+  
+  var checkList2 = document.getElementById('list2');
+  var items2 = document.getElementById('items2');
   checkList2.getElementsByClassName('anchor')[0].onclick = function (evt) {
     if (items2.classList.contains('visible')){
       items2.classList.remove('visible');
@@ -52,9 +49,9 @@ var items2 = document.getElementById('items2');
   items2.onblur = function(evt) {
     items2.classList.remove('visible');
   }
-
-var checkList3 = document.getElementById('list3');
-var items3 = document.getElementById('items3');
+  
+  var checkList3 = document.getElementById('list3');
+  var items3 = document.getElementById('items3');
   checkList3.getElementsByClassName('anchor')[0].onclick = function (evt) {
     if (items3.classList.contains('visible')){
       items3.classList.remove('visible');
@@ -68,4 +65,3 @@ var items3 = document.getElementById('items3');
   items3.onblur = function(evt) {
     items3.classList.remove('visible');
   }
-
